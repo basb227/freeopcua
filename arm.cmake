f815bcd6431d0be8f0ab3ef5ffabb88999c34cf5
basb227 <56951242+basb227@users.noreply.github.com>
2020-01-09 09:52:39 +0100

    Create arm.cmake
    arm toolchain added

---
+++ arm.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(tools $ENV{HOME}/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf)
+set(CMAKE_C_COMPILER ${tools}/bin/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/bin/arm-linux-gnueabihf-g++)
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,--no-as-needed -static")
+set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+
+set(BOOST_ROOT $ENV{HOME}/boost_1_64_0)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
